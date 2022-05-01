15b69c99f7b1644da1e0c813d24ec4ad8fdd83ef
Izabela <izabelabertozo@hotmail.com>
2022-05-01 18:45:35 -0300

    exercicios

---
+++ exercicio12/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Um motorista de taxi deseja calcular o rendimento de seu carro na praça. Sabendo-se que o preço do combustível é de R$6,90, escreva um algoritmo para ler a 
+marcação do odômetro (marcador de quilometragem) no início do dia, a marcação no final do dia, o número de litros de combustível gasto e o valor total (R$) recebido 
+dos passageiros. Calcule e escreva a média do consumo em Km/l e o lucro líquido do dia.
+
+Fórmulas: Total quilometragem =  marcação odômetro final do dia - marcação odômetro inicio do dia 
+
+Média do consumo = Total quilometragem/ quantidade de combustível gasto
+
+Lucro do dia = Valor total recebido - (quantidade de combustível gasto * 6,90)
+*/
+
+Console.WriteLine("Exercicio 12 ");
+
+double quilometragem;
+double odometro1;
+double odometro2;
+double litros;
+double gastod;
+double recebidodia;
+double consumo;
+double lucrodia;
+double consumo1;
+
+
+
+Console.WriteLine("Digite a quilometrage inicial ");
+odometro1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor total recebidos pelos passageiros do dia ");
+recebidodia = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade de litros gasto no dia ");
+litros = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a quilometragem final ");
+odometro2 = double.Parse(Console.ReadLine());
+
+quilometragem = odometro1 - odometro2;
+
+consumo = quilometragem / litros * (-1);
+
+consumo1 = litros * 6.90;
+
+lucrodia = recebidodia - consumo1;
+
+Console.WriteLine(" A media de consumo é " + consumo + "km/l"+ " O lucro dia foi R$" + lucrodia);
+
+
+
+
+
+
+
+
---
+++ exercicio13/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Uma loja vende bicicletas com um acréscimo de 50% sobre o preço de custo. Ela paga a cada vendedor 2 salários mínimos mensais, mais uma comissão de 15% sobre o 
+preço de custo de cada bicicleta vendida. Desenvolva um algoritmo que leia o valor do salário mínimo, o preço de custo de cada bicicleta e o número de bicicletas 
+vendidas pelo vendedor, calcule e mostre: o salário do empregado
+*/
+
+
+Console.WriteLine("Exercicio 13");
+
+double salario;
+double comissao;
+double custo;
+double venda;
+double venda1;
+double salariofinal;
+
+Console.WriteLine(" Qual o Salario minimo? ");
+salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" Qual o custo? ");
+custo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantas vendas foram realizadas? ");
+venda = double.Parse(Console.ReadLine());
+
+comissao= custo * 0.15;
+
+venda1 = comissao * venda;
+
+salariofinal = salario * 2 + venda1;
+
+Console.WriteLine("O salario final é " + salariofinal);
+
+
+
